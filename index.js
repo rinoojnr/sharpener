adc325369bfb1fd4f569a6c75948c9b75ec453bb
rinoojnr <rinoojnr@gmail.com>
2023-10-05 15:52:58 +0530

    Added getElementsByClassName styles

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ var headertext = document.getElementById('main-headding');
 headertext.style.borderBottom='solid 3px #000';
 headertext.innerHTML ='<b>Malayalam</b>';
 headertext.style.color ='green'
+
+
+var items = document.getElementsByClassName('item')
+// console.log(items[2])
+// items[1].textContent ="Hello";
+// items[1].style.fontWeight ="bold";
+// items[1].style.backgroundColor="yellow"
+
+// items.style.backgroundColor="grey"  //=========== it gives an error because of html collection
+// for(let i=0;i<items.length;i++){
+//     items[i].style.backgroundColor="grey"
+// }
+items[2].style.backgroundColor="green"
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold"
+}
